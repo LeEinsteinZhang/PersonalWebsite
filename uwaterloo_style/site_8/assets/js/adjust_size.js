f1e87ae0d34566a741d862705315b638c8a91d9d
lez <leeinsteinzhang@gmail.com>
2023-10-22 18:53:24 -0400

    update js

--- uwaterloo_style/site_8/assets/js/adjust_size.js
+++
@@ -1,9 +0,0 @@
-const element = document.getElementById('resizableElement');
-const initialHeight = window.innerHeight;
-const targetHeight = 1000;
-
-window.addEventListener('scroll', function() {
-    let scrollY = window.scrollY;
-    let newHeight = Math.max(targetHeight, initialHeight - scrollY);
-    element.style.height = `${newHeight}px`;
-});
--- uwaterloo_style/site_8/assets/js/phone.js
+++
@@ -1,3 +0,0 @@
-document.querySelector(".dropdown-btn").addEventListener("click", function() {
-  document.querySelector(".dropdown-content").classList.toggle("show");
-});
