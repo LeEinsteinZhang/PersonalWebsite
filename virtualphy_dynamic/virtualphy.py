1178253905b2e6deb524139057880ed59fb652d8
Le Zhang <leeinsteinzhang@gmail.com>
2024-07-23 20:06:54 -0400

    update entry file

--- virtualphy_dynamic/virtualphy.py
+++ virtualphy_dynamic/virtualphy.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
 
 app = Flask(__name__)
 
-app.url_map.strict_slashes = False  # 禁用严格斜杠
+app.url_map.strict_slashes = False
 
 def load_translation(page, lang):
     with open(os.path.join('translations', page, f'{lang}.json'), 'r', encoding='utf-8') as file:
@@ -12,16 +12,17 @@ def load_translation(page, lang):
 
 @app.route('/')
 def home():
-    lang = 'en'  # 默认语言为英文
-    translations = load_translation('index', lang)
-    return render_template('index.html', translations=translations, lang=lang)
+    user_lang = request.accept_languages.best_match(['zh', 'en'])
+    if user_lang is None or user_lang not in ['zh', 'en']:
+        user_lang = 'en'
+    return redirect(url_for('home_lang', lang=user_lang))
 
-@app.route('/<lang>/')
+@app.route('/<lang>')
 def home_lang(lang):
     translations = load_translation('index', lang)
     return render_template('index.html', translations=translations, lang=lang)
 
-@app.route('/<lang>/career/')
+@app.route('/<lang>/career')
 def career_lang(lang):
     translations = load_translation('career', lang)
     return render_template('career.html', translations=translations, lang=lang)
