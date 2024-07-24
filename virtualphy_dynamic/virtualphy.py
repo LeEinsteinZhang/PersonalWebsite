b029a15a37c5f740e2c5b93999301eb7a53e4abf
Le Zhang <leeinsteinzhang@gmail.com>
2024-07-23 20:22:06 -0400

    update the index settings

--- virtualphy_dynamic/virtualphy.py
+++ virtualphy_dynamic/virtualphy.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import json
 import os
 
 app = Flask(__name__)
-
+app.secret_key = '233'
 app.url_map.strict_slashes = False
 
 def load_translation(page, lang):
@@ -12,35 +12,50 @@ def load_translation(page, lang):
 
 @app.route('/')
 def home():
-    user_lang = request.accept_languages.best_match(['zh', 'en'])
-    if user_lang is None or user_lang not in ['zh', 'en']:
-        user_lang = 'en'
-    return redirect(url_for('home_lang', lang=user_lang))
+    if 'lang' in session:
+        lang = session['lang']
+    else:
+        lang = request.accept_languages.best_match(['zh', 'en'])
+        if lang is None or lang not in ['zh', 'en']:
+            lang = 'en'
+        session['lang'] = lang
+    return redirect(url_for('home_lang', lang=lang))
 
 @app.route('/<lang>')
 def home_lang(lang):
+    if 'lang' not in session or session['lang'] != lang:
+        session['lang'] = lang
     translations = load_translation('index', lang)
     return render_template('index.html', translations=translations, lang=lang)
 
 @app.route('/<lang>/career')
 def career_lang(lang):
+    if 'lang' not in session or session['lang'] != lang:
+        session['lang'] = lang
     translations = load_translation('career', lang)
     return render_template('career.html', translations=translations, lang=lang)
 
 @app.route('/<lang>/career/modeling_engineer')
 def job_modeling_engineer(lang):
+    if 'lang' not in session or session['lang'] != lang:
+        session['lang'] = lang
     translations = load_translation('job/modeling_engineer', lang)
     return render_template('job.html', translations=translations, lang=lang)
 
 @app.route('/<lang>/career/scripts_developer')
 def job_scripts_developer(lang):
+    if 'lang' not in session or session['lang'] != lang:
+        session['lang'] = lang
     translations = load_translation('job/scripts_developer', lang)
     return render_template('job.html', translations=translations, lang=lang)
 
 @app.route('/<lang>/career/web_developer')
 def job_web_developer(lang):
+    if 'lang' not in session or session['lang'] != lang:
+        session['lang'] = lang
     translations = load_translation('job/web_developer', lang)
     return render_template('job.html', translations=translations, lang=lang)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
