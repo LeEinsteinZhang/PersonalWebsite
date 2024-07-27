83984b54e44630f84864f41342b3d8641786003b
Le Zhang <leeinsteinzhang@gmail.com>
2024-07-26 22:42:44 -0400

    update logic remove the prefix of the different language pages

--- zhangle_dynamic/static/js/language.js
+++ zhangle_dynamic/static/js/language.js
@@ -1,7 +1,10 @@
 // JavaScript Document
-var userLang = navigator.language || navigator.userLanguage; 
-if (userLang.includes("zh")) {
-    window.location.href = '/cn/';
-} else {
-    window.location.href = '/en/';
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var switchLink = document.getElementById("lang-switch");
+    var currentUrl = window.location.href;
+    var hashIndex = currentUrl.indexOf("#");
+    if (hashIndex !== -1) {
+        var hash = currentUrl.substring(hashIndex);
+        switchLink.href += hash;
+    }
+});
