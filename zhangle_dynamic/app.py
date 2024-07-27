9a6b48abedae110f83dba2d3fb400c3b1e22d767
Le Zhang <leeinsteinzhang@gmail.com>
2024-07-26 22:49:27 -0400

    update logic

--- zhangle_dynamic/app.py
+++ zhangle_dynamic/app.py
@@ -6,10 +6,11 @@ app = Flask(__name__)
 app.secret_key = '233'
 app.url_map.strict_slashes = False
 
+default_lang = 'en'
+
 def load_content(page, lang):
     with open(os.path.join('content', page, f'{lang}.json'), 'r', encoding='utf-8') as file:
         content = json.load(file)
-    # Load footer content
     with open(os.path.join('content', 'base', f'{lang}.json'), 'r', encoding='utf-8') as file:
         footer_content = json.load(file)
     content.update(footer_content)
@@ -17,38 +18,33 @@ def load_content(page, lang):
 
 @app.route('/')
 def home():
-    if 'lang' in session:
-        lang = session['lang']
-    else:
-        lang = request.accept_languages.best_match(['zh', 'en'])
-        if lang is None or lang not in ['zh', 'en']:
-            lang = 'en'
-        session['lang'] = lang
-    return redirect(url_for('home_lang', lang=lang))
-
-@app.route('/<lang>')
-def home_lang(lang):
-    if 'lang' not in session or session['lang'] != lang:
-        session['lang'] = lang
+    if 'lang' not in session:
+        session['lang'] = default_lang
+    lang = session['lang']
     content = load_content('index', lang)
-    return render_template('index.html', content=content, lang=lang, current_page='index')
+    return render_template('index.html', content=content, lang=lang, current_page='index', default_lang=default_lang)
+
+@app.route('/switch_lang/<lang>')
+def switch_lang(lang):
+    session['lang'] = lang
+    next_url = request.args.get('next', url_for('home'))
+    return redirect(next_url)
 
 @app.route('/resume/<filename>')
 def download_resume(filename):
     return send_from_directory(os.path.join(app.root_path, 'static/resume'), filename)
 
-
-@app.route('/<lang>/<experience>')
-def experiences(lang, experience):
-    if 'lang' not in session or session['lang'] != lang:
-        session['lang'] = lang
+@app.route('/<experience>')
+def experiences(experience):
+    if 'lang' not in session:
+        session['lang'] = default_lang
+    lang = session['lang']
     content = load_content(f'experiences/{experience}', lang)
-    return render_template('experience.html', content=content, lang=lang, current_page=experience)
+    return render_template('experience.html', content=content, lang=lang, current_page=experience, default_lang=default_lang)
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/icons'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
 
 if __name__ == '__main__':
     app.run()
--- zhangle_dynamic/static/js/language.js
+++ zhangle_dynamic/static/js/language.js
@@ -1,10 +1,11 @@
-// JavaScript Document
 document.addEventListener("DOMContentLoaded", function() {
     var switchLink = document.getElementById("lang-switch");
-    var currentUrl = window.location.href;
-    var hashIndex = currentUrl.indexOf("#");
-    if (hashIndex !== -1) {
-        var hash = currentUrl.substring(hashIndex);
-        switchLink.href += hash;
+    if (switchLink) {
+        var currentUrl = window.location.href;
+        var hashIndex = currentUrl.indexOf("#");
+        if (hashIndex !== -1) {
+            var hash = currentUrl.substring(hashIndex);
+            switchLink.href += hash;
+        }
     }
 });
