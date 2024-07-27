adf7e863c0633fff0619cbd4a7af0082feef94ac
Le Zhang <leeinsteinzhang@gmail.com>
2024-07-27 02:01:56 -0400

    create one file for js

--- zhangle_dynamic/static/js/language.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('lang-switch').addEventListener('click', function(e) {
-    var hash = window.location.hash;
-    var langSwitchHref = e.currentTarget.getAttribute('href');
-    if (hash) {
-        e.currentTarget.setAttribute('href', langSwitchHref + hash);
-    }
-})
