6003bf5c05d6f613d8921b3f33a6f4379ceef1c8
Le Zhang <leeinsteinzhang@gmail.com>
2024-10-03 01:04:55 -0400

    Fix typo

--- ZhangLe/ZhangLe/obj/Debug/net8.0/ZhangLe.AssemblyInfo.cs
+++ ZhangLe/ZhangLe/obj/Debug/net8.0/ZhangLe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZhangLe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27384dfc2200179f9a0e811e745c72fb7b45ab4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03c04af3ee1a62cfbb2fbb51a1eaf85948a0540")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZhangLe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZhangLe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
