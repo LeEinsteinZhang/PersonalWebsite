29b60710f3595777c656b7d027c1b99e1c012ba9
Le Zhang <le.zhang@uwaterloo.ca>
2024-11-14 16:01:44 -0500

    Add asp.net mvc template

---
+++ zhangle_asp_dot_net/ZhangLe/ZhangLe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ZhangLe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
