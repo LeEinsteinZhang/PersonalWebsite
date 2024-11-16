ecfe418f855a985ba4b163d6cf9cff3b9b9c29c1
Le Zhang <le.zhang@uwaterloo.ca>
2024-11-16 00:04:45 -0500

    move magic number to constant

---
+++ zhangle_asp_dot_net/ZhangLe/ZhangLe/Constant.cs
@@ -0,0 +1,11 @@
+﻿namespace ZhangLe.Constant
+{
+    public class Constant
+    {
+        public const int maxNumberOfAwards = 5;
+        public const int maxNumberOfExperiences = 10;
+        public const int maxNumberOfProjects = 10;
+        public const int maxNumberOfImagesInRow = 2;
+        public const int maxNumberOfSections = 2;
+    }
+}
