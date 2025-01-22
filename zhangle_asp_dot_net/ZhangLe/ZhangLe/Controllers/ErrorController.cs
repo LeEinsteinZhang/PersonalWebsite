d02dbfd5ac7cf684835ec516453f3f474f323198
Le Zhang <leeinsteinzhang@gmail.com>
2025-01-22 11:59:29 -0500

    Add ErrorController.cs to ZhangLe ASP.NET

---
+++ zhangle_asp_dot_net/ZhangLe/ZhangLe/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class ErrorController : Controller
+{
+    [Route("Error/{code}")]
+    public IActionResult GeneralError(int code)
+    {
+        Response.StatusCode = code;
+        ViewData["StatusCode"] = code;
+
+        string errorMessage = code switch
+        {
+            404 => "Page Not Found",
+            500 => "Internal Server Error",
+            403 => "Forbidden",
+            _ => "An unexpected error occurred"
+        };
+
+        ViewData["ErrorMessage"] = errorMessage;
+
+        return View("Error");
+    }
+}
--- zhangle_asp_dot_net/ZhangLe/ZhangLe/Program.cs
+++ zhangle_asp_dot_net/ZhangLe/ZhangLe/Program.cs
@@ -8,10 +8,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error/500");
     app.UseHsts();
 }
+
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
